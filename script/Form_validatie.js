a7f2230b8e4256ea6b2d1a3322c2d53eeb216a9c
VantommeJaron <jaron.vantomme@student.howest.be>
2022-10-12 00:21:04 +0200

    floating label and like button

---
+++ script/Form_validatie.js
@@ -0,0 +1,90 @@
+let password  = {};
+let email = {};
+let hart = {};
+
+
+function checkInput(){
+    console.log("error")
+}
+
+function enableListerners(){
+  // Email check
+  email.input.addEventListener('blur', function () {
+    // Kijkt of er iets ingevuld is
+    if (this.value.length < 1) {
+      console.log('vul tekstvak in');
+      email.field.classList.add('has-error');
+      email.message.classList.remove('no-value-error-gone');
+    }
+    // Kijkt of er een @ in zit
+    else if (!this.value.includes('@')) {
+      console.log('Er staat geen @');
+      email.field.classList.add('has-error');
+      email.message.classList.add('no-value-error-gone');
+      
+    }
+    // correct
+    else {
+      console.log('correct');
+      email.field.classList.remove('has-error');
+      email.message.classList.add('no-value-error-gone');
+      
+    }
+  });
+
+  // Password check
+  password.input.addEventListener('blur', function () {
+    // Kijkt of er iets ingevuld is
+    if (this.value.length < 1) {
+      console.log('vul tekstvak in');
+      password.field.classList.add('has-error');
+      password.message.classList.remove('no-value-error-gone');
+
+    }
+    // correct
+    else {
+      console.log('correct');
+      password.field.classList.remove('has-error');
+      document.getElementById('password-error').innerHTML = 'Password';
+      password.message.classList.add('no-value-error-gone');
+
+    }
+  });
+}
+
+
+function handleFloatingLabel() {
+  email.input.addEventListener('blur', function () {
+    if (this.value.length < 1) {
+      console.log('leeg');
+      email.label.classList.remove('c-label-with-error-no-text');
+    } else {
+      email.label.classList.add('c-label-with-error-no-text');
+    }
+  });
+  
+  // Password check
+  password.input.addEventListener('blur', function () {
+    if (this.value.length < 1) {
+      password.label.classList.remove('c-label-with-error-no-text');
+    }
+    // correct
+    else {
+      password.label.classList.add('c-label-with-error-no-text');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Je Moeder!');
+    password.input = document.querySelector('.js-password-input');
+    password.field = document.querySelector('.js-password-field');
+    password.message = document.querySelector('.js-password-error');
+    password.label = document.querySelector('.js-password-label');
+    email.input = document.querySelector('.js-email-input');
+    email.field = document.querySelector('.js-email-field');
+    email.message = document.querySelector('.js-email-error');
+    email.label = document.querySelector('.js-email-label');
+    enableListerners();
+    handleFloatingLabel();
+});
--- script/app.js
+++ script/app.js
@@ -1,13 +1,51 @@
-function handleFloatingLabel() {
-   
-}
+const password = {};
+let email = {};
+let hart = {};
 
 function handlePasswordSwitcher() {
-    
+  password.input = document.querySelector('.js-password-input');
+  password.toggle = document.querySelector('.js-password-toggle');
+  password.hide = document.querySelector('.c-toggle-password__icon--hide');
+  password.show = document.querySelector('.c-toggle-password__icon--show');
+
+  password.toggle.addEventListener('change', function () {
+    if (password.toggle.checked) {
+      password.input.type = 'input';
+      password.hide.classList.add('o-hide-accessible');
+      password.show.classList.remove('o-hide-accessible');
+    } else {
+      password.input.type = 'password';
+      password.hide.classList.remove('o-hide-accessible');
+      password.show.classList.add('o-hide-accessible');
+    }
+  });
+}
+
+function hartswitcher() {
+  // Hart check
+  hart.toggle = document.querySelector('.js-hart-toggle');
+  hart.like = document.querySelector('.js-like-butn');
+   hart.toggle.addEventListener('change', function () {
+     if (hart.toggle.checked) {
+       console.log("druk1")
+       hart.like.classList.add('c-toggle-hart__icon-like');
+       
+      } else {
+        console.log("druk2")
+        hart.like.classList.remove('c-toggle-hart__icon-like');
+     }
+   });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+  console.log('Je Moeder!');
+  password.input = document.querySelector('.js-password-input');
+  password.field = document.querySelector('.js-password-field');
+  password.message = document.querySelector('.js-password-error');
+  email.input = document.querySelector('.js-email-input');
+  email.field = document.querySelector('.js-email-field');
+  email.message = document.querySelector('.js-email-error');
+  handleFloatingLabel();
+  handlePasswordSwitcher();
+  hartswitcher();
 });
